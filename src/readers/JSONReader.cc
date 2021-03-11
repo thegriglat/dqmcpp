4b7e223ebb24fd8a1bfef3318a975f84b7286ae1
Grigory Latyshev <thegriglat@gmail.com>
2021-03-11 14:14:52 +0300

    Update JSONReader

--- src/readers/JSONReader.cc
+++ src/readers/JSONReader.cc
@@ -6,8 +6,32 @@
  */
 
 #include "JSONReader.hh"
+#include <cmath>
 #include <vector>
 #include "../common/logging.hh"
+
+namespace {
+using namespace dqmcpp::ECAL;
+/**
+ * @brief Get iz of ECAL from histo title
+ *
+ * @param title
+ */
+DETECTORS getECALDetector(const std::string& title) {
+  if (title.find("EB") != title.npos) {
+    return DETECTORS::EB;
+  }
+  if (title.find("EE+") != title.npos || title.find("EE +") != title.npos) {
+    return DETECTORS::EEPLUS;
+  }
+  if (title.find("EE-") != title.npos || title.find("EE -") != title.npos) {
+    return DETECTORS::EEMINUS;
+  }
+  WARNING("Cannot determine ECAL iz from title");
+  return DETECTORS::EB;
+}
+
+}  // namespace
 
 namespace dqmcpp {
 namespace readers {
@@ -24,75 +48,36 @@ bool JSONReader::isValid(nlohmann::json& j) {
     return false;
   return true;
 }
-/**
- * @brief Get iz of ECAL from histo title
- *
- * @param title
- */
-static ECAL::DETECTORS getECALDetector(const std::string& title) {
-  if (title.find("EB") != title.npos) {
-    return ECAL::DETECTORS::EB;
-  }
-  if (title.find("EE+") != title.npos || title.find("EE +") != title.npos) {
-    return ECAL::DETECTORS::EEPLUS;
-  }
-  if (title.find("EE-") != title.npos || title.find("EE -") != title.npos) {
-    return ECAL::DETECTORS::EEMINUS;
-  }
-  WARNING("Cannot determine ECAL iz from title");
-  return ECAL::DETECTORS::EB;
-}
 
 std::vector<ECAL::ChannelData> JSONReader::parse(nlohmann::json& j) {
   using namespace std;
-  using BinContentList = vector<vector<double>>;
   // check validity of json
-  if (!isValid(j))
+  const auto data2d = parse2D(j);
+  if (data2d.size() == 0)
     return std::vector<ECAL::ChannelData>();
   const auto iz = getECALDetector(j["hist"]["title"].get<string>());
+  vector<ECAL::ChannelData> channel_data;
   const auto xaxis = j["hist"]["xaxis"];
-  const auto yaxis = j["hist"]["yaxis"];
   const auto xtitle = xaxis["title"].get<string>();
-  const auto xfirst = xaxis["first"]["value"].get<int>();
-  const auto xlast = xaxis["last"]["value"].get<int>();
   const char xsign = (xtitle.find('-') != xtitle.npos) ? -1 : 1;
-  const auto xstep = ((xlast > xfirst) ? 1 : -1);
-
-  const auto ytitle = yaxis["title"].get<string>();
-  const auto yfirst = yaxis["first"]["value"].get<int>();
-  const auto ylast = yaxis["last"]["value"].get<int>();
-  const auto ystep = (ylast > yfirst) ? 1 : -1;
-
-  // bin increment always == 1 as it is channel
-  const auto content = j["hist"]["bins"]["content"].get<BinContentList>();
-  std::vector<ECAL::ChannelData> channel_data;
-  channel_data.reserve(ECAL::NEBChannels);  // as max of EB and EE+- n channels
-  int ybin = 0;
-  for (int iy = yfirst; iy != ylast; iy += ystep) {
-    int xbin = 0;
-    for (int ix = xfirst; ix != xlast; ix += xstep) {
-      const auto value = content.at(ybin).at(xbin);
-      int ix_iphi;
-      int iy_ieta;
-      if (xtitle != "ix") {
-        // in case of barrel we have to swap x and y values
-        // horrible....
-        ix_iphi = std::abs(iy) - ((yfirst < 0) ? 1 : 0);
-        iy_ieta = (ix)*xsign;
-      } else {
-        ix_iphi = ix;
-        iy_ieta = iy;
-      }
-      ECAL::Channel c(ix_iphi + 1, iy_ieta + xsign /*sign(iy_ieta)*/, iz);
-      ECAL::ChannelData cd(c, value);
-      if (value != 0) {
-        // to avoid overlapping DQM output for different SM
-        channel_data.push_back(cd);
-      }
-      xbin++;
+  for (auto& datapoint : data2d) {
+    const int x = std::trunc(datapoint.x);
+    const int y = std::trunc(datapoint.y);
+    int ix_iphi;
+    int iy_ieta;
+    if (iz == ECAL::DETECTORS::EB) {
+      // EB
+      // in case of barrel we have to swap x and y values
+      ix_iphi = std::abs(y) + 1;
+      iy_ieta = (x + 1) * xsign;
+    } else {
+      // EE
+      ix_iphi = x + 1;
+      iy_ieta = y + 1;
     }
-    ybin++;
-  };
+    ECAL::Channel c(ix_iphi, iy_ieta, iz);
+    channel_data.push_back(ECAL::ChannelData(c, datapoint.value));
+  }
   return channel_data;
 }
 
