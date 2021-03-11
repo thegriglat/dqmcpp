f62fbcbab7d624258f449cca6dba7178ba42277d
Grigory Latyshev <thegriglat@gmail.com>
2021-03-12 01:21:58 +0300

    Split L1TEGammaIsoPre to EB, EE, EB+EE

--- src/plugins/L1TEGammaIsoPre.cc
+++ src/plugins/L1TEGammaIsoPre.cc
@@ -6,6 +6,7 @@
 #include "L1TEGammaIsoPre.hh"
 #include <algorithm>
 #include <fstream>
+#include <functional>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -31,10 +32,14 @@ string urls(const int run, const std::string& dataset) {
       "egamma_noniso_bx_ieta_firstbunch_ptmin10p0";
   return net::DQMURL::dqmurl(run, dataset, path);
 }
-vector<ECAL::Data2D> filter(const vector<ECAL::Data2D>& data, const double x) {
+
+vector<ECAL::Data2D> filter(
+    const vector<ECAL::Data2D>& data,
+    std::function<bool(const ECAL::Data2D&)> filter_fn) {
   vector<ECAL::Data2D> r;
+  r.reserve(data.size());
   for (auto& e : data)
-    if (std::abs(e.x - x) < 0.1)
+    if (filter_fn(e))
       r.push_back(e);
   return r;
 }
@@ -50,8 +55,12 @@ vector<RunProb> calcProb(const vector<ECAL::RunData2D>& rundata) {
   vector<RunProb> result;
   result.reserve(rundata.size());
   for (auto& rd : rundata) {
-    auto minus1values = filter(rd.data, -1);
-    auto zerovalues = filter(rd.data, 0);
+    // -1 bin
+    auto minus1values = filter(
+        rd.data, [](const ECAL::Data2D& d) { return std::abs(d.x + 1) < 0.1; });
+    // zero bin
+    auto zerovalues = filter(
+        rd.data, [](const ECAL::Data2D& d) { return std::abs(d.x) < 0.1; });
     auto minus1_i = sum(minus1values);
     auto zero_i = sum(zerovalues);
     auto prob = minus1_i / (minus1_i + zero_i);
@@ -60,12 +69,16 @@ vector<RunProb> calcProb(const vector<ECAL::RunData2D>& rundata) {
   return result;
 }
 
-vector<ECAL::Data2D> filter0m2(const vector<ECAL::Data2D>& data) {
-  vector<ECAL::Data2D> r;
-  for (auto& e : data)
-    if (e.x > -1.5 && e.x < 0.5)
-      r.push_back(e);
-  return r;
+vector<ECAL::Data2D> filter0m1(const vector<ECAL::Data2D>& data) {
+  return filter(data,
+                [](const ECAL::Data2D& d) { return d.x > -1.5 && d.x < 0.5; });
+}
+
+vector<ECAL::Data2D> filter_det(const vector<ECAL::Data2D>& data, bool isEB) {
+  auto filter_fn =
+      (isEB) ? [](const ECAL::Data2D& d) { return std::abs(d.y) < 40; }
+             : [](const ECAL::Data2D& d) { return std::abs(d.y) >= 40; };
+  return filter(data, filter_fn);
 }
 
 }  // namespace
@@ -74,6 +87,9 @@ namespace plugins {
 
 void L1TEGammaIsoPrePlugin::Process() {
   vector<ECAL::RunData2D> rundata;
+  vector<ECAL::RunData2D> eb_rundata;
+  vector<ECAL::RunData2D> ee_rundata;
+
   rundata.reserve(runListReader->runs().size());
   for (auto& run : runListReader->runs()) {
     auto url = urls(run.runnumber, run.dataset);
@@ -81,22 +97,54 @@ void L1TEGammaIsoPrePlugin::Process() {
     auto content = reader->parse2D(reader->get(url));
     if (content.size() == 0)
       continue;
-    content = filter0m2(content);
+    content = filter0m1(content);
     rundata.push_back(ECAL::RunData2D(run.runnumber, content));
+    auto eb_content = filter_det(content, true);
+    auto ee_content = filter_det(content, false);
+    eb_rundata.push_back(ECAL::RunData2D(run.runnumber, eb_content));
+    ee_rundata.push_back(ECAL::RunData2D(run.runnumber, ee_content));
   }
   vector<RunProb> runprob = calcProb(rundata);
-  ofstream out("l1.plt");
-  std::sort(runprob.begin(), runprob.end(),
-            [](const RunProb& a, const RunProb& b) { return a.run < b.run; });
+  vector<RunProb> eb_runprob = calcProb(eb_rundata);
+  vector<RunProb> ee_runprob = calcProb(ee_rundata);
+
+  auto sorter = [](const RunProb& a, const RunProb& b) {
+    return a.run < b.run;
+  };
+  std::sort(runprob.begin(), runprob.end(), sorter);
+  std::sort(eb_runprob.begin(), eb_runprob.end(), sorter);
+  std::sort(ee_runprob.begin(), ee_runprob.end(), sorter);
   writers::Gnuplot1DWriter::Data1D data;
-  for (auto& rp : runprob) {
+  writers::Gnuplot1DWriter::Data1D eb_data;
+  writers::Gnuplot1DWriter::Data1D ee_data;
+
+  for (auto& rp : runprob)
     data.push_back({std::to_string(rp.run), rp.prob});
-  }
+  for (auto& rp : eb_runprob)
+    eb_data.push_back({std::to_string(rp.run), rp.prob});
+  for (auto& rp : ee_runprob)
+    ee_data.push_back({std::to_string(rp.run), rp.prob});
+
+  ofstream out("L1TEGammaIsoPre_ECAL.plt");
   writers::Gnuplot1DWriter writer(data);
-  writer.setTitle("L1TEGammaIsoPre");
-  writer.setOutput("L1TEGammaIsoPre.png");
+  writer.setTitle("L1TEGammaIsoPre ECAL");
+  writer.setOutput("L1TEGammaIsoPre_ECAL.png");
   out << writer << std::endl;
   out.close();
+
+  ofstream outeb("L1TEGammaIsoPre_EB.plt");
+  writers::Gnuplot1DWriter writereb(eb_data);
+  writereb.setTitle("L1TEGammaIsoPre EB");
+  writereb.setOutput("L1TEGammaIsoPre_EB.png");
+  outeb << writereb << std::endl;
+  outeb.close();
+
+  ofstream outee("L1TEGammaIsoPre_EE.plt");
+  writers::Gnuplot1DWriter writeree(ee_data);
+  writeree.setTitle("L1TEGammaIsoPre EE");
+  writeree.setOutput("L1TEGammaIsoPre_EE.png");
+  outee << writeree << std::endl;
+  outee.close();
 }
 
 }  // namespace plugins
