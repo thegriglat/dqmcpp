2a1e6538f053783e47f9a96998d0d96b29128fe6
Grigory Latyshev <thegriglat@gmail.com>
2021-03-18 12:11:50 +0300

    Add subanalyses

--- src/plugins/RMSChannels.cc
+++ src/plugins/RMSChannels.cc
@@ -6,6 +6,7 @@
  */
 #include <cmath>
 #include <fstream>
+#include <functional>
 #include <map>
 #include <string>
 #include "../common/common.hh"
@@ -17,16 +18,9 @@ using namespace dqmcpp;
 using namespace std;
 
 namespace {
-const double RMSMAX = 8.0;
-const auto rmslimit = [](const double rms) { return rms > RMSMAX; };
 
-}  // namespace
-
-namespace dqmcpp {
-namespace plugins {
-
-std::vector<std::string> RMSPlugin::urls(const unsigned int runnumber,
-                                         const std::string& dataset) {
+std::vector<std::string> urls(const unsigned int runnumber,
+                              const std::string& dataset) {
   std::vector<std::string> urls;
   char* plot = new char[72];
   for (int i = -18; i < 19; ++i) {
@@ -50,15 +44,17 @@ std::vector<std::string> RMSPlugin::urls(const unsigned int runnumber,
   delete[] plot;
   return urls;
 }
+
 // channel and number of channel's repetitions
 using BadChannel = std::pair<ECAL::Channel, int>;
-std::vector<ECAL::RunChannelData> RMSPlugin::analyze(
-    const std::vector<ECAL::RunChannelData>& rundata) {
+std::vector<ECAL::RunChannelData> analyze(
+    const std::vector<ECAL::RunChannelData>& rundata,
+    std::function<bool(double)> checkfn) {
   vector<BadChannel> badchannels_list;
   badchannels_list.reserve(100);
   for (auto& e : rundata) {
     for (auto& channeldata : e.data) {
-      if (rmslimit(channeldata.value)) {
+      if (checkfn(channeldata.value)) {
         auto it = std::find_if(badchannels_list.begin(), badchannels_list.end(),
                                [&channeldata](const BadChannel& bc) {
                                  return bc.first == channeldata.channel;
@@ -93,7 +89,10 @@ std::vector<ECAL::RunChannelData> RMSPlugin::analyze(
   }
   return rd;
 }
-void RMSPlugin::plot(const std::vector<ECAL::RunChannelData>& rundata) {
+
+void plot(const std::vector<ECAL::RunChannelData>& rundata,
+          const std::string& filename,
+          const std::string& title) {
   if (rundata.size() == 0)
     return;
 
@@ -109,24 +108,31 @@ void RMSPlugin::plot(const std::vector<ECAL::RunChannelData>& rundata) {
       data.insert({{runstr, channelstr}, chd.value});
     }
   }
-  std::ofstream out("rms_channels.plt");
+  std::ofstream out(filename + ".plt");
   writers::Gnuplot2DWriter writer(data);
-  writer.setTitle("G12 RMS channels");
+  writer.setTitle(title);
   writer.setZ(0, 100);
   writer.setPalette(colors::PaletteSets::RMSHeatMap);
   writer.setZ(0, 10);
   writer.setZTick(1);
-  writer.setOutput("rms_channels.png");
+  writer.setOutput(filename + ".png");
   out << writer << std::endl;
   out.close();
   return;
 }
 
+}  // namespace
+
+namespace dqmcpp {
+namespace plugins {
+
 void RMSPlugin::Process() {
   using namespace std;
   vector<ECAL::RunChannelData> rundata;
+  const auto nruns = runListReader->runs().size();
+  int i = 1;
   for (auto& run : runListReader->runs()) {
-    cout << run.runnumber << endl;
+    cout << run.runnumber << " [" << i++ << "/" << nruns << "]" << endl;
     vector<ECAL::ChannelData> data;
     data.reserve(ECAL::NTotalChannels);
     for (auto& url : urls(run.runnumber, run.dataset)) {
@@ -137,7 +143,20 @@ void RMSPlugin::Process() {
     ECAL::RunChannelData rd(run, data);
     rundata.push_back(rd);
   }
-  plot(analyze(rundata));
+  typedef struct {
+    std::string filename;
+    std::string title;
+    std::function<bool(double)> func;
+  } Analysis;
+  vector<Analysis> analyses = {
+      {"rms_channels_greater_8", "ECAL G12 RMS > 8",
+       [](const double rms) { return rms > 8.0; }},
+      {"rms_channels_less_2", "ECAL G12 RMS < 1.5",
+       [](const double rms) { return rms < 1.5 && rms > 0; }}};
+  for (auto& analy : analyses) {
+    std::cout << "## Run subanalysis " << analy.title << std::endl;
+    plot(analyze(rundata, analy.func), analy.filename, analy.title);
+  }
 }
 
 }  // namespace plugins
--- src/plugins/RMSChannels.hh
+++ src/plugins/RMSChannels.hh
@@ -10,13 +10,6 @@ namespace dqmcpp {
 namespace plugins {
 
 class RMSPlugin : public Plugin {
- private:
-  std::vector<std::string> urls(const unsigned int runnumber,
-                                const std::string& dataset);
-  std::vector<ECAL::RunChannelData> analyze(
-      const std::vector<ECAL::RunChannelData>& rundata);
-  void plot(const std::vector<ECAL::RunChannelData>& rundata);
-
  public:
   void Process(void);
 };
