e394a709cda96d33c8a1840fc16b4a07a7a9672c
Grigory Latyshev <thegriglat@gmail.com>
2021-03-18 12:17:02 +0300

    Use sized types

--- src/ecalchannels/ECALChannels.hh
+++ src/ecalchannels/ECALChannels.hh
@@ -16,32 +16,32 @@ namespace ECALChannels {
  * Dumped from DQM a long long time ago ...
  */
 struct ChannelInfo {
-  unsigned short fed;          // 601..654
-  unsigned char tcc;           // 1..108
-  unsigned char tower;         // 1..68
-  unsigned char stripintower;  // 1..5
-  unsigned char xtalinstrip;   // 1..5
-  unsigned char ccu;           // 1..68
-  unsigned char vfe;           // 1..5
-  unsigned char xtalinvfe;     // 1..5
-  unsigned char xtalinccu;     // 0..24
-  signed short ieta;           //-999..85
-  signed short iphi;           //-999..360
-  signed short ix;             //-999..100
-  signed short iy;             //-999..100
-  signed char iz;              // -1..1
-  unsigned short hashedid;     // 0..61199
-  unsigned short ic;           // 1..1700
-  unsigned int cmsswid;
-  unsigned int dbid;
-  signed char ietatt;     // -28..28
-  unsigned char iphitt;   // 1..72
-  unsigned char tccch;    // 1..68
-  unsigned char tccslot;  // 2..17
-  unsigned char slbch;    // 1..8
-  unsigned char slbslot;  // 1..9
-  unsigned char ietagct;  // 4..17
-  unsigned char iphigct;  // 0..17
+  uint16_t fed;          // 601..654
+  uint8_t tcc;           // 1..108
+  uint8_t tower;         // 1..68
+  uint8_t stripintower;  // 1..5
+  uint8_t xtalinstrip;   // 1..5
+  uint8_t ccu;           // 1..68
+  uint8_t vfe;           // 1..5
+  uint8_t xtalinvfe;     // 1..5
+  uint8_t xtalinccu;     // 0..24
+  int16_t ieta;          //-999..85
+  int16_t iphi;          //-999..360
+  int16_t ix;            //-999..100
+  int16_t iy;            //-999..100
+  int8_t iz;             // -1..1
+  uint16_t hashedid;     // 0..61199
+  uint16_t ic;           // 1..1700
+  uint32_t cmsswid;
+  uint32_t dbid;
+  int8_t ietatt;    // -28..28
+  uint8_t iphitt;   // 1..72
+  uint8_t tccch;    // 1..68
+  uint8_t tccslot;  // 2..17
+  uint8_t slbch;    // 1..8
+  uint8_t slbslot;  // 1..9
+  uint8_t ietagct;  // 4..17
+  uint8_t iphigct;  // 0..17
   // std::string crate;
   std::string det() const;
 };
