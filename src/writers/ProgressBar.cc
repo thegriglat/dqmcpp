211069492b427dd9695368f984b0806fdc115e36
Grigory Latyshev <thegriglat@gmail.com>
2021-03-21 22:54:26 +0300

    Add remaining time calculation

--- src/writers/ProgressBar.cc
+++ src/writers/ProgressBar.cc
@@ -1,9 +1,32 @@
 #include "ProgressBar.hh"
 
 #include <cmath>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 
 using namespace std;
+
+namespace {
+std::string formatTime(int secs) {
+  int minutes = secs / 60;
+  int seconds = secs % 60;
+  int hours = minutes / 60;
+  minutes = minutes % 60;
+  stringstream ss;
+  ss << setfill('0') << setw(2) << hours << ":" << setfill('0') << setw(2)
+     << minutes << ":" << setfill('0') << setw(2) << seconds;
+  return ss.str();
+}
+std::string formatPercent(float value) {
+  int a = std::trunc(value);
+  int b = std::trunc((value * 10 - a * 10));
+  stringstream ss;
+  ss << setfill(' ') << setw(3) << a << "." << b;
+  return ss.str();
+}
+
+}  // namespace
 
 namespace dqmcpp {
 namespace writers {
@@ -12,14 +35,33 @@ void ProgressBar::finish(void) {
   cout << endl;
 }
 
+float ProgressBar::progress() const {
+  return static_cast<float>(currentProgress) /
+         static_cast<float>(maxProgressValue);
+}
+
+// return estimated time in seconds
+int ProgressBar::getEstimatedTime() {
+  auto _end = std::chrono::steady_clock::now();
+  const auto seconds_passed =
+      static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(
+                              _end - _beginTime)
+                              .count()) /
+      1000;
+  auto events_passed = currentProgress % updateTimingEvery;
+  if (events_passed == 0) {
+    events_passed = updateTimingEvery;
+  }
+  const auto time_per_event = seconds_passed / events_passed;
+  int secs = time_per_event * (maxProgressValue - currentProgress);
+  secs = std::max(0, secs);
+  return secs;
+}
+
 void ProgressBar::draw(void) {
   cout << label << " ";
   cout << "[";
-  const float progress = static_cast<float>(currentProgress) /
-                         static_cast<float>(maxProgressValue);
-  //   cout << "progress " << progress << " cp " << currentProgress << " mp "
-  //    << maxProgressValue << endl;
-  const int pos = barWidth * progress;
+  const int pos = barWidth * progress();
   for (int i = 0; i < barWidth; ++i) {
     if (i < pos)
       std::cout << "=";
@@ -28,7 +70,8 @@ void ProgressBar::draw(void) {
     else
       std::cout << " ";
   }
-  std::cout << "] " << std::ceil(progress * 100.0 * 10.0) / 10.0 << " %\r";
+  std::cout << "] " << formatPercent(progress() * 100.0) << " % "
+            << formatTime(getEstimatedTime()) << "\r";
   std::cout.flush();
 }
 
--- src/writers/ProgressBar.hh
+++ src/writers/ProgressBar.hh
@@ -1,5 +1,7 @@
 #ifndef PROGRESSBAR_HH
 
+#include <array>
+#include <chrono>
 #include <string>
 
 namespace dqmcpp {
@@ -8,23 +10,37 @@ namespace writers {
 class ProgressBar {
  private:
   static constexpr int barWidth = 70;
+  static constexpr int updateTimingEvery = 20;
   int currentProgress = 0;
   int maxProgressValue = 1;
   std::string label = "";
+  std::chrono::steady_clock::time_point _beginTime;
   void draw(void);
   void finish(void);
+  float progress() const;
+  int getEstimatedTime();
 
  public:
-  ProgressBar(const int maxSteps = 1) : maxProgressValue(maxSteps){};
+  ProgressBar(const int maxSteps = 1)
+      : maxProgressValue(maxSteps),
+        _beginTime(std::chrono::steady_clock::now()){};
+
   inline void update(int value) {
     currentProgress = value;
     draw();
+    if (currentProgress % updateTimingEvery == 0) {
+      _beginTime = std::chrono::steady_clock::now();
+    }
   };
+
   inline void increment(const int increment = 1.0) {
     update(currentProgress + increment);
   };
+
   inline void setMaxValue(const int maxvalue) { maxProgressValue = maxvalue; }
+
   inline void setLabel(const std::string& newlabel) { label = newlabel; }
+
   ~ProgressBar() { finish(); };
 };
 }  // namespace writers
