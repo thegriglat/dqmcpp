e5d1034f7681a3161844aa6a4c9c4bc6da268d94
Grigory Latyshev <thegriglat@gmail.com>
2021-03-22 15:15:41 +0300

    Draw in 2d

--- src/plugins/TPOccupancyL1.cc
+++ src/plugins/TPOccupancyL1.cc
@@ -3,13 +3,15 @@
 #include <algorithm>
 #include <iostream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 #include "../ECAL/ECAL.hh"
+#include "../colors/Colors.hh"
 #include "../common/common.hh"
 #include "../common/gnuplot.hh"
 #include "../net/DQMURLProvider.hh"
-#include "../writers/Gnuplot1DWriter.hh"
+#include "../writers/Gnuplot2DWriter.hh"
 #include "../writers/ProgressBar.hh"
 
 using namespace std;
@@ -21,25 +23,12 @@ const string geturl(const ECAL::Run& run) {
   return net::DQMURL::dqmurl(run, "L1T/L1TStage2CaloLayer1/ecalOccupancy");
 }
 
-vector<std::pair<std::string, double>> getData(
-    const vector<plugins::TPOccupancyL1::RunL1Data>& rundata,
-    int x,
-    int y) {
-  vector<pair<string, double>> result;
-  result.reserve(rundata.size());
-  for (auto& rd : rundata) {
-    const auto tt1 = find_if(
-        rd.data.begin(), rd.data.end(),
-        [x, y](const ECAL::Data2D& d2d) { return d2d.x == x && d2d.y == y; });
-    if (tt1 != rd.data.end()) {
-      const auto ally = common::filter(
-          rd.data, [x](const ECAL::Data2D& d2d) { return d2d.x == x; });
-      const auto avgy = common::median(
-          ally, [](const ECAL::Data2D& d2d) { return d2d.value; });
-      result.push_back({to_string(rd.run.runnumber), (tt1->value) / (avgy)});
-    }
-  }
-  return result;
+double getAvgY(const plugins::TPOccupancyL1::RunL1Data& rd, const int x) {
+  const auto ally = common::filter(
+      rd.data, [x](const ECAL::Data2D& d2d) { return d2d.x == x; });
+  const auto avgy =
+      common::median(ally, [](const ECAL::Data2D& d2d) { return d2d.value; });
+  return avgy;
 }
 
 }  // namespace
@@ -63,28 +52,94 @@ vector<TPOccupancyL1::RunL1Data> TPOccupancyL1::getRunData(void) {
 
 void TPOccupancyL1::Process() {
   // plot
-  const auto rundata = getRunData();
-  writers::ProgressBar progress((28 * 2));
+  auto rundata = getRunData();
+  for (auto& rd : rundata) {
+    for (int x = -28; x <= 28; ++x) {
+      if (x == 0)
+        continue;
+      // get avg
+      const double avgy = getAvgY(rd, x);
+      // scale to avgy
+      for (auto& d : rd.data) {
+        if (d.x == x) {
+          d.value /= avgy;
+        }
+      }
+    }
+    // now we have scaled rundata
+  }
+
+  // determine median against all runs and get list of bad elements
+  set<pair<int, int>> badxy;
+  writers::ProgressBar* progress = new writers::ProgressBar(28 * 2);
+  progress->setLabel("x iterations");
   for (int x = -28; x <= 28; ++x) {
     if (x == 0)
       continue;
-    progress.increment();
     for (int y = 1; y <= 72; ++y) {
-      const auto data = getData(rundata, x, y);
-      if (data.size() == 0)
-        continue;
-      writers::Gnuplot1DWriter writer(data);
-      const string name = "TPOccupancyL1_" + to_string(x) + "_" + to_string(y);
-      writer.setOutput(name + ".png");
-      writer.setTitle(name);
-      writer.setY(0, 6);
-      writer.with("linespoints");
-      ofstream out(name + ".plt");
-      out << writer;
-      out.close();
-      //   common::gnuplot::exec(writer);
+      vector<double> runsvalues;
+      runsvalues.reserve(rundata.size());
+      for (auto& rd : rundata) {
+        auto it = std::find_if(rd.data.begin(), rd.data.end(),
+                               [x, y](const ECAL::Data2D& d2d) {
+                                 return d2d.x == x && d2d.y == y;
+                               });
+        if (it != rd.data.end()) {
+          runsvalues.push_back(it->value);
+        }
+      }
+      // get median
+      const double median = common::median(runsvalues);
+      const double upperlimit = median * 2;
+      const double lowerlimit = median / 2;
+      for (auto& rd : rundata) {
+        for (auto& d : rd.data) {
+          if (d.x != x || d.y != y)
+            continue;
+          if (d.value > upperlimit || d.value < lowerlimit)
+            badxy.insert({x, y});
+        }
+      }
     }
+    progress->increment();
   }
+  delete progress;
+  progress = new writers::ProgressBar(rundata.size());
+  // now we have list of bad [x,y]
+  writers::Gnuplot2DWriter::Data2D data;
+  double _maxvalue = -1;
+  for (auto& rd : rundata) {
+    progress->setLabel("writing " + to_string(rd.run.runnumber));
+    for (auto& p : badxy) {
+      const auto x = p.first;
+      const auto y = p.second;
+      auto it = std::find_if(
+          rd.data.begin(), rd.data.end(),
+          [x, y](const ECAL::Data2D& d2d) { return d2d.x == x && d2d.y == y; });
+      if (it != rd.data.end()) {
+        const string xlabel = to_string(rd.run.runnumber);
+        const string ylabel = common::string_format("[%d,%d]", x, y);
+        const auto value = it->value;
+        _maxvalue = std::max(value, _maxvalue);
+        data.insert({{xlabel, ylabel}, value});
+      }
+    }
+    progress->increment();
+  }
+  delete progress;
+  writers::Gnuplot2DWriter writer(data);
+  writer.setOutput("TPOccupancyL1.png");
+  writer.setTitle("TPOccupancyL1");
+  writer.setZ(0, _maxvalue);
+  writer.setPalette({{0.0, colors::ColorSets::black},
+                     {0.0, colors::ColorSets::blue},
+                     {0.5 / _maxvalue, colors::ColorSets::white},
+                     {2.0 / _maxvalue, colors::ColorSets::white},
+                     {2.0 / _maxvalue, colors::ColorSets::yellow},
+                     {1.0, colors::ColorSets::red}});
+  ofstream out("TPOccupancyL1.plt");
+  out << writer;
+  out.close();
   return;
 }
 
