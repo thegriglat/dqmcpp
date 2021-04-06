a8aebf13955ff71fe4fa78fa39f297ce343995c1
Grigory Latyshev <thegriglat@gmail.com>
2021-04-06 16:30:30 +0300

    use ZeroSuppressionRate histogram (to be upgraded)

--- src/PFGplugins/LowInterestTT.cc
+++ src/PFGplugins/LowInterestTT.cc
@@ -28,9 +28,20 @@ string url(const ECAL::Run& run) {
   return net::DQMURL::dqmurl(
       run, "EcalBarrel/EBSelectiveReadoutTask/EBSRT low interest TT Flags");
 }
+string zero_suppresion_url(const ECAL::Run& run) {
+  return net::DQMURL::dqmurl(
+      run,
+      "EcalBarrel/EBSelectiveReadoutTask/EBSRT zero suppression 1 SR Flags");
+}
 
 void plot(const vector<ECAL::RunTTData>& rundata) {
+  writers::ProgressBar progress(rundata.size());
+  progress.setLabel("plotting...");
   writers::Gnuplot2DWriter::Data2D data;
+  vector<string> runss;
+  for (auto rd : rundata) {
+    runss.push_back(to_string(rd.run.runnumber));
+  }
   for (auto& rd : rundata) {
     const auto xlabel = to_string(rd.run.runnumber);
     for (auto& tt : rd.data) {
@@ -39,8 +50,10 @@ void plot(const vector<ECAL::RunTTData>& rundata) {
           common::string_format("%s TT%02d", det.c_str(), tt.tt);
       data.insert({{xlabel, ylabel}, tt.value});
     }
+    progress.increment();
   }
   writers::Gnuplot2DWriter writer(data);
+  writer.setXlabels(runss);
   writer.setOutput("LowInterestTT.png");
   writer.setTitle("LowInterestTT");
   writer.setZ(0.0, 1.0);
@@ -61,9 +74,21 @@ void dqmcpp::plugins::LowInterestTT::Process() {
     pb.setLabel(to_string(run.runnumber));
     auto content = ECAL::channel2TT(ECAL::Data2D2ChannelData(
         readers::JSONReader::parse2D(readers::JSONReader::get(url(run)))));
+    auto zscontent =
+        ECAL::channel2TT(ECAL::Data2D2ChannelData(readers::JSONReader::parse2D(
+            readers::JSONReader::get(zero_suppresion_url(run)))));
     auto removeit = std::remove_if(
-        content.begin(), content.end(),
-        [](const ECAL::TTData& ttd) { return ttd.value > LOWLIMIT; });
+        content.begin(), content.end(), [&zscontent](const ECAL::TTData& ttd) {
+          bool hastoremove = ttd.value > LOWLIMIT && ttd.value < HIGHLIMIT;
+          auto it = std::find_if(
+              zscontent.begin(), zscontent.end(),
+              [&ttd](const ECAL::TTData& zsttd) { return zsttd.sameTT(ttd); });
+          if (it != zscontent.end()) {
+            if (!hastoremove && it->value > 0.98)
+              hastoremove = true;
+          }
+          return hastoremove;
+        });
     content.erase(removeit, content.end());
     rundata.emplace_back(run, content);
     pb.increment();
