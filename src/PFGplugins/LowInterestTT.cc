69a0830315a129479a2f4d116c664d31cfd58232
Grigory Latyshev <thegriglat@gmail.com>
2021-04-06 15:52:07 +0300

    Show only runs < LOWLIMIT

--- src/PFGplugins/LowInterestTT.cc
+++ src/PFGplugins/LowInterestTT.cc
@@ -38,10 +38,6 @@ void plot(const vector<ECAL::RunTTData>& rundata) {
       const auto ylabel =
           common::string_format("%s TT%02d", det.c_str(), tt.tt);
       data.insert({{xlabel, ylabel}, tt.value});
-      if (tt.value < LOWLIMIT) {
-        cout << rd.run.runnumber << " " << det << " TT" << tt.tt << " = "
-             << tt.value << endl;
-      }
     }
   }
   writers::Gnuplot2DWriter writer(data);
@@ -49,7 +45,7 @@ void plot(const vector<ECAL::RunTTData>& rundata) {
   writer.setTitle("LowInterestTT");
   writer.setZ(0.0, 1.0);
   writer.setPalette(colors::PaletteSets::Rainbow);
-  writer.setZTick(0.001);
+  writer.setZTick(0.1);
   ofstream out("LowInterestTT.plt");
   out << writer;
   out.close();
@@ -66,9 +62,8 @@ void dqmcpp::plugins::LowInterestTT::Process() {
     auto content = ECAL::channel2TT(ECAL::Data2D2ChannelData(
         readers::JSONReader::parse2D(readers::JSONReader::get(url(run)))));
     auto removeit = std::remove_if(
-        content.begin(), content.end(), [](const ECAL::TTData& ttd) {
-          return ttd.value < HIGHLIMIT && ttd.value > LOWLIMIT;
-        });
+        content.begin(), content.end(),
+        [](const ECAL::TTData& ttd) { return ttd.value > LOWLIMIT; });
     content.erase(removeit, content.end());
     rundata.emplace_back(run, content);
     pb.increment();
