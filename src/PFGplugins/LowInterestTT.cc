d010ac4958b3fe5679c94b7fa0fe563d8108122b
Grigory Latyshev <thegriglat@gmail.com>
2021-04-07 10:41:17 +0300

    Update LowInterestTT, remove ZS cut

--- src/PFGplugins/LowInterestTT.cc
+++ src/PFGplugins/LowInterestTT.cc
@@ -12,7 +12,6 @@
 #include "writers/ProgressBar.hh"
 
 #define LOWLIMIT (0.9)
-#define HIGHLIMIT (0.9995)
 
 REGISTER_PLUGIN(LowInterestTT)
 
@@ -27,11 +26,6 @@ using namespace dqmcpp;
 string url(const ECAL::Run& run) {
   return net::DQMURL::dqmurl(
       run, "EcalBarrel/EBSelectiveReadoutTask/EBSRT low interest TT Flags");
-}
-string zero_suppresion_url(const ECAL::Run& run) {
-  return net::DQMURL::dqmurl(
-      run,
-      "EcalBarrel/EBSelectiveReadoutTask/EBSRT zero suppression 1 SR Flags");
 }
 
 void plot(const vector<ECAL::RunTTData>& rundata) {
@@ -74,22 +68,9 @@ void dqmcpp::plugins::LowInterestTT::Process() {
     pb.setLabel(to_string(run.runnumber));
     auto content = ECAL::channel2TT(ECAL::Data2D2ChannelData(
         readers::JSONReader::parse2D(readers::JSONReader::get(url(run)))));
-    auto zscontent =
-        ECAL::channel2TT(ECAL::Data2D2ChannelData(readers::JSONReader::parse2D(
-            readers::JSONReader::get(zero_suppresion_url(run)))));
     auto removeit = std::remove_if(
-        content.begin(), content.end(), [&zscontent](const ECAL::TTData& ttd) {
-          bool hastoremove = ttd.value > LOWLIMIT && ttd.value < HIGHLIMIT;
-          auto it = std::find_if(zscontent.begin(), zscontent.end(),
-                                 [&ttd](const ECAL::TTData& zsttd) {
-                                   return zsttd.base == ttd.base;
-                                 });
-          if (it != zscontent.end()) {
-            if (!hastoremove && it->value > 0.98)
-              hastoremove = true;
-          }
-          return hastoremove;
-        });
+        content.begin(), content.end(),
+        [](const ECAL::TTData& ttd) { return ttd.value > LOWLIMIT; });
     content.erase(removeit, content.end());
     rundata.emplace_back(run, content);
     pb.increment();
