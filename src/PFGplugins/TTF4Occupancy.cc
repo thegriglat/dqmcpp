4e46e471c25365330ef4f6d314754fe86a1eb678
Grigory Latyshev <thegriglat@gmail.com>
2021-04-19 16:08:57 +0300

    Simplify url getter

--- src/PFGplugins/TTF4Occupancy.cc
+++ src/PFGplugins/TTF4Occupancy.cc
@@ -33,20 +33,21 @@ std::vector<URLType> urls(const unsigned int runnumber,
                           const std::string& dataset) {
   std::vector<URLType> urls;
   for (int i = -1; i < 2; ++i) {
-    string det = "Barrel";
-    string d = "EB";
-    string suf = "";
-    if (i != 0) {
-      // endcap
-      det = "Endcap";
-      d = "EE";
-      suf = (i == 1) ? " EE +" : " EE -";
+    if (i == 0) {
+      // eb
+      urls.push_back(
+          URLType(net::DQMURL::dqmurl(
+                      runnumber, dataset,
+                      "EcalBarrel/EBTriggerTowerTask/EBTTT TTF4 Occupancy"),
+                  true));
+    } else {
+      const std::string pm = (i == 1) ? "EE +" : "EE -";
+      urls.push_back(URLType(
+          net::DQMURL::dqmurl(
+              runnumber, dataset,
+              "EcalEndcap/EETriggerTowerTask/EETTT TTF4 Occupancy " + pm),
+          false));
     }
-    string eeplot = "Ecal";
-    eeplot +=
-        det + "/" + d + "TriggerTowerTask/" + d + "TTT TTF4 Occupancy" + suf;
-    urls.push_back(
-        URLType(net::DQMURL::dqmurl(runnumber, dataset, eeplot), i == 0));
   }
   return urls;
 };
