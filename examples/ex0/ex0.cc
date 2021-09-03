ed1f443634e52dc42fa8e56becd9384ea963517b
Grigory Latyshev <thegriglat@gmail.com>
2021-09-03 10:00:58 +0300

    simple example

---
+++ examples/ex0/ex0.cc
@@ -0,0 +1,19 @@
+#include "ex0.hh"
+
+#include <iostream>
+
+REGISTER_PLUGIN(Example0)
+
+using namespace std;
+using namespace dqmcpp;
+
+namespace dqmcpp{
+
+namespace plugins {
+
+    void Example0::Process(){
+        cout << "Hello, DQMCpp!" << endl;
+    }
+}
+
+}
---
+++ examples/ex0/ex0.hh
@@ -0,0 +1,14 @@
+#ifndef EX0_HH
+#include "plugins/Plugins.hh"
+
+namespace dqmcpp {
+namespace plugins {
+class Example0 : public Plugin {
+ public:
+  void Process();
+};
+}  // namespace plugins
+}  // namespace dqmcpp
+
+#define EX0_HH
+#endif
