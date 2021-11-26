e23a6323017771ced8a9c99f2998f943fe1ff5b3
Grigory Latyshev <thegriglat@gmail.com>
2021-11-26 14:56:32 +0300

    update shell

--- report_build.sh
+++ report_build.sh
@@ -1,9 +1,10 @@
 #/bin/bash
 
+builddir="$1"
+outdir="$2"
+mkdir -p $outdir
 
-mkdir -p public
-
-cp -f build/*.png public
-cp custom.css  public/
-python3 generate_md.py pngmap.json build/ | pandoc -f markdown -t html -s -c custom.css --metadata title="DQMReport" > public/index.html
-cd public; ls *.png | xargs -n 1 -P 4 -I {} convert {} -trim {}; cd -
+cp -f $builddir/*.png $outdir
+cp custom.css  $outdir/
+python3 generate_md.py pngmap.json $builddir/ | pandoc -f markdown -t html -s -c custom.css --metadata title="DQMReport" > $outdir/index.html
+cd $outdir; ls *.png | xargs -n 1 -P 4 -I {} convert {} -trim {}; cd -
