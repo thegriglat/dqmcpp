0307ec36d59791256be2c5f681684522ab180821
Grigory Latyshev <thegriglat@gmail.com>
2021-11-26 10:54:33 +0300

    Add convert trim

--- hugobuild.sh
+++ hugobuild.sh
@@ -6,3 +6,4 @@ mkdir -p public
 cp -f build/*.png public
 cp custom.css  public/
 python3 hugo_generate.py pngmap.json build/ | pandoc -f markdown -t html -s -c custom.css > public/index.html
+cd public; ls *.png | xargs -n 1 -P 4 -I {} convert {} -trim {}; cd -
