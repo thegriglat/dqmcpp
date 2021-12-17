c55f493c66b24ff793566e57049cf147b6fe85b5
Grigory Latyshev <thegriglat@gmail.com>
2021-12-17 09:23:53 +0300

    Deployed f4b0833 with MkDocs version: 1.2.2

--- api/search/functions_3.js
+++ api/search/functions_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['data2d2channel_347',['Data2D2Channel',['../namespacedqmcpp_1_1ECAL.html#ac3fb18a1e1231aa345e7d5b68650cbb7',1,'dqmcpp::ECAL']]],
   ['data2d2channeldata_348',['Data2D2ChannelData',['../namespacedqmcpp_1_1ECAL.html#a3a674b3d82620a22e78961886889c2ec',1,'dqmcpp::ECAL']]],
-  ['descrtiption_349',['descrtiption',['../classdqmcpp_1_1plugins_1_1Plugin.html#a1171e20a2b8e891aa9ec1401b002851b',1,'dqmcpp::plugins::Plugin']]],
+  ['description_349',['description',['../classdqmcpp_1_1plugins_1_1Plugin.html#a1d9166bfe8011d5118c1ccc1c2f3f02a',1,'dqmcpp::plugins::Plugin']]],
   ['det_350',['det',['../structdqmcpp_1_1ECALChannels_1_1ChannelInfo.html#a79dab6f28335993ddb720ee9ed8b24de',1,'dqmcpp::ECALChannels::ChannelInfo']]],
   ['det_5fiz_351',['det_iz',['../structdqmcpp_1_1ECALChannels_1_1ChannelInfo.html#a8d9238066f15f53b6b22f18684f19ad7',1,'dqmcpp::ECALChannels::ChannelInfo']]],
   ['dirname_352',['dirname',['../namespacedqmcpp_1_1common.html#a32a587626a9dc7a9b6ef3a93f2588447',1,'dqmcpp::common']]],
